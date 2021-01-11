cc52e7ead37cc2757273ef1bcd9eeaf2c4d5c610
danielisgr8 <schubertmdaniel@gmail.com>
2021-01-11 09:55:19 -0800

    Basic page setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Layout, Menu, Dropdown } from "antd";
+import { UserOutlined } from "@ant-design/icons";
+import "antd/dist/antd.css";
+import "./App.css";
 
-function App() {
+const { Header, Content } = Layout;
+
+const App = () => {
+  const menu = (
+    <Menu>
+      <Menu.Item key="1">
+        nav 1
+      </Menu.Item>
+      <Menu.Item key="2">
+        nav 2
+      </Menu.Item>
+      <Menu.Item key="3">
+        nav 3
+      </Menu.Item>
+      <Menu.Item key="4">
+        nav 4
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout className="App">
+      <Header className="header" >
+        <Dropdown overlay={menu} trigger={["click"]}>
+          <UserOutlined style={{ position: "absolute", left: "0.5rem" }}/>
+        </Dropdown>
+        <p>Confidential Claus</p>
+      </Header>
+      <Layout>
+        <Content>
+          <p>Content</p>
+        </Content>
+      </Layout>
+    </Layout>
   );
 }
 
